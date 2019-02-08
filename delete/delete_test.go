7478dba93d7afc0984dd8e653b4df68922d12480
Qing Miao <cdutwhu@outlook.com>
2019-02-08 17:04:22 +1100

    * clean up

--- delete/delete_test.go
+++ delete/delete_test.go
@@ -13,6 +13,6 @@ func TestN3LoadConfig(t *testing.T) {
 func TestDelete(t *testing.T) {
 	defer func() { PH(recover(), Cfg.Global.ErrLog) }()
 	TestN3LoadConfig(t)
-	n := Sif("9269671A-BB89-4281-B20D-668C1D7FFD05") /* context must end with '-sif' */
+	n := Sif("D3E34F41-9D75-101A-8C3D-00AA001A1652") //"9269671A-BB89-4281-B20D-668C1D7FFD05") /* context must end with '-sif' */
 	fPln(n)
 }
