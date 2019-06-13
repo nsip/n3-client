7bf83b7e8eab342a05028214893444146326f10d
Qing Miao <cdutwhu@outlook.com>
2019-06-12 23:27:33 -0700

    clean up

--- send/send_test.go
+++ send/send_test.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	c "../config"
+	pp "../preprocess"
 )
 
 func TestJunk(t *testing.T) {
@@ -25,22 +26,10 @@ func TestToNode(t *testing.T) {
 	defer func() { PH(recover(), CFG.Global.ErrLog) }()
 	TestN3LoadConfig(t)
 
-	datafile := "../inbound/hsie/geography/stage3/content.json" //             *** change file ***
-	data := string(Must(ioutil.ReadFile(datafile)).([]byte))
-
-	// ***
-	// data = u.FmtJSONStr(data, "../util/")
-	// if ok := checkColon(data); !ok {
-	// 	t.Errorf("%s has colon in field value, fix them before sending", datafile)
-	// 	return
-	// }
-	// if ok := checkHyphen(data); !ok {
-	// 	t.Errorf("%s has hyphen in field value, fix them before sending", datafile)
-	// 	return
-	// }
-	// ***
-
-	IDs, _, _, _ := ToNode(data, "id", "hsie") //                        *** change <idmark>, <default root> ***
+	file := "../inbound/hsie/history/stage5/overview.json" //           *** change <file> ***
+	data := string(Must(ioutil.ReadFile(file)).([]byte))
+	data = pp.FmtJSONStr(data, "../preprocess/util/", "./") //           *** format json string ***
+	IDs, _, _, _ := ToNode(data, "id", "hsie")              //           *** change <idmark>, <default root> ***
 	time.Sleep(1 * time.Second)
 	for _, id := range IDs {
 		fPln("sent:", id)
